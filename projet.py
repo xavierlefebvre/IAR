02e481b53de7bd0738ade08aed3cfe78943018db
xavierlefebvre <xavier.lefebvre.v1@gmail.com>
2015-12-18 18:52:30 +0100

    simplification du code déjà existant

--- projet.py
+++ projet.py
@@ -2,38 +2,33 @@ import numpy as np
 import random
 import math
 
-#position du cercle (left/right)
-#rayon/diametre du cercle (big/small)
-#forme du robot : 7 longueurs
-#position du robot : 14 angles
-#controller = 14 neurones + connections
-#h=-10 # (hight) : ordonnee du centre du cercle
 
-l=-10 # left
-r=10  # right
-b=4   # big
-s=2   # small
+# robot
+# 7 bras
+# controller = 14 neurones + connections
+
+# position du cercle
+l=-10  # left        (abscisse)
+r=10   # right       (abscisse)
+lh=-10 # left hight  (ordonnee)
+rh=-10 # right hight (ordonnee)
+# rayon du cercle
+b=4    # big
+s=2    # small
 
 def initNeurons(env):
 	neurons = np.ones((7,2), dtype=np.int)
 	if env[0] == l:
-		neurons[:,0] = np.multiply(-1,neurons[:,0])
+		neurons[:,0] *= -1
 	if env[1] == b:
-		neurons[:,1] = np.multiply(-1,neurons[:,1])
-	return neurons.reshape((neurons.size,)).tolist()
+		neurons[:,1] *= -1
+	return neurons.reshape((neurons.size,))
+
+def applyNoise(neurons):
+	return neurons * np.concatenate( (np.ones((1,14),dtype=int), np.ones(14,dtype=int) - 2 * np.identity(14,dtype=int)), axis=0 )
 
 def getDesiredAngles(neurons):
-	angles = []
-	for dof in np.array(neurons).reshape((len(neurons)/2,2)):
-		if   np.array_equal(dof,[-1,-1]):
-			angles.append(-45)
-		elif np.array_equal(dof,[-1, 1]):
-			angles.append(-15)
-		elif np.array_equal(dof,[ 1,-1]):
-			angles.append( 15)
-		elif np.array_equal(dof,[ 1, 1]):
-			angles.append( 45)
-	return angles
+	return np.dot( np.array([30,15]), neurons.reshape((len(neurons)/2,2)).T)
 
 def getXY(armsLengths,angles):
 	angles = np.cumsum(angles)
@@ -54,23 +49,13 @@ def getXY(armsLengths,angles):
 		rightFingers.append([x,y])
 	return arms+leftFingers[1:]+rightFingers[1:]
 
-def getInitialPercepts(envs):
-	initConditions = np.array([[ initNeurons(env) for env in envs ]])
-	for i in range(14):
-		noisyPercepts = np.copy(initConditions[0])
-		noisyPercepts[:,i] = - noisyPercepts[:,i]
-		initConditions = np.concatenate( (initConditions,[noisyPercepts]) )
-	return initConditions
-
-def createRandomNetwork(neurons,connectionNb):
+def getRandomController(neurons,connectionNb):
 	connections = []
+	neuronsList = np.arange(len(neurons))
 	while connectionNb > 0:
-		neuronsList = np.arange(len(neurons)).tolist()
-		origin = random.choice(neuronsList)
-		neuronsList.remove(origin)
-		destination = random.choice(neuronsList)
-		if not( (origin,destination) in connections):
-			connections.append([origin,destination])
+		connection = random.sample(neuronsList,2)
+		if not( connection in connections):
+			connections.append(connection)
 			connectionNb -= 1
 	return (neurons,connections)
 
@@ -103,7 +88,7 @@ def runTests():
 		[ 45, 45, 45, 45, 45, 45, 45]]
 	output = []
 	for neurons in neuronsInput:
-		output.append( getDesiredAngles(neurons) )
+		output.append( getDesiredAngles(np.array(neurons)) )
 	if np.array_equal(expected,output):
 		print("OK \t getDesiredAngles")
 	else:
@@ -128,3 +113,8 @@ def runTests():
 		print("ERROR \t getXY")
 
 runTests()
+
+envs = [(l,b),(l,s),(r,b),(r,s)]
+ic = np.array( [applyNoise(initNeurons(env)) for env in envs] )
+
+
